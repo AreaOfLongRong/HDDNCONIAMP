3064b6203598a2cc8f9f6f15d67a9f82d648c9e1
AreaOfLongRong <zhaoyameng1106@126.com>
2017-10-25 22:33:34 +0800

    1、修改完善了帮助文档，补充了前期未完成的部分截图； 2、将软件授权日期调整为10月27日。

--- HDDNCONIAMP/FormMain.cs
+++ HDDNCONIAMP/FormMain.cs
@@ -89,7 +89,7 @@ namespace HDDNCONIAMP
         /// <summary>
         /// 程序截止日期
         /// </summary>
-        private DateTime DEADLINE = new DateTime(2017, 10, 28);
+        private DateTime DEADLINE = new DateTime(2017, 10, 27);
 
         /// <summary>
         /// GIS定位关联视频控件
